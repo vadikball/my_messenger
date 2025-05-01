29e5c7c75305c306b18a548486cba1714f70db00
Vadim Goncharov <psinahoi@gmail.com>
2025-05-01 12:21:33 +0200

    add linting utils

---
+++ scripts/format.sh
@@ -0,0 +1,5 @@
+autoflake --remove-unused-variables --remove-duplicate-keys --ignore-init-module-imports --remove-all-unused-imports -i -r app tests
+black app tests
+isort --profile black app tests
+mypy app tests
+flake8 app tests
---
+++ scripts/lint.sh
@@ -0,0 +1,4 @@
+black --check --diff app tests
+isort --profile black --check-only app tests
+mypy app tests
+flake8 app tests
