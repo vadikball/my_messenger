2d6b4a92727342ae4455ce524a6339247c17d381
Vadim Goncharov <psinahoi@gmail.com>
2025-05-04 10:35:53 +0200

    add migration

--- app/db/models/chats.py
+++ app/db/models/chats.py
@@ -1,7 +1,9 @@
+from datetime import datetime
+from typing import List
 from uuid import uuid4
 
-from sqlalchemy import ForeignKey, SmallInteger, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import DateTime, ForeignKey, SmallInteger, String, func
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from app.constants.base import DEFAULT_STRING_LENGTH
 from app.db.declarative_base import Base
@@ -15,6 +17,12 @@ class ChatsModel(Base):
     id: mapped_uuid = mapped_column(default_postgresql_uuid_factory(), primary_key=True, default=uuid4)
     name: mapped_str = mapped_column(String(DEFAULT_STRING_LENGTH), nullable=False)
     type: Mapped[int] = mapped_column(SmallInteger, nullable=False)
+    chat_members: Mapped[List["ChatMembersModel"]] = relationship(
+        cascade="all, delete-orphan",
+        lazy="dynamic",
+        passive_deletes=True,
+        order_by="chat_members.created_at",
+    )
 
 
 class ChatMembersModel(Base):
@@ -26,3 +34,4 @@ class ChatMembersModel(Base):
     user_id: mapped_uuid = mapped_column(
         default_postgresql_uuid_factory(), ForeignKey(UsersModel.id, ondelete="CASCADE"), primary_key=True
     )
+    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), nullable=False, server_default=func.now())
